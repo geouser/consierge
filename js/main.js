022b94b3e1d7605970d879b7b88e7246bd8970b7
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2017-01-04 10:46:30 +0200

    h

--- js/main.js
+++ js/main.js
@@ -123,6 +123,30 @@ jQuery(document).ready(function($) {
     })
 
 
+    jQuery('#rev_slider_1').show().revolution({
+        /* options are 'auto', 'fullwidth' or 'fullscreen' */
+        sliderLayout: 'auto',
+        /* basic navigation arrows and bullets */
+        navigation: {
+            arrows: {
+                enable: false,
+                style: 'hesperiden',
+                hide_onleave: false
+            },
+            bullets: {
+                enable: false,
+                style: 'hesperiden',
+                hide_onleave: false,
+                h_align: 'center',
+                v_align: 'bottom',
+                h_offset: 0,
+                v_offset: 20,
+                space: 5
+            }
+        }
+    });
+
+
 
     /*---------------------------
                                   Magnific popup
